49b7df418b65f6188ede94b559232e4300ff3c5e
Willian Felipe <w.felipebraz@gmail.com>
2022-07-12 10:54:39 -0300

    7

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,13 +1,56 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
 export default class Album extends Component {
-  render() {
-    return (
-      <div data-testid="page-album">
-        Album
-        <Header />
-      </div>
-    );
+  constructor() {
+    super();
+    this.state = {
+      artista: '',
+      infos: [],
+    };
   }
+
+  componentDidMount() {
+    this.pegaMusica();
+  }
+
+    pegaMusica = async () => {
+      const { match: { params: { id } } } = this.props;
+      // const { infos, artista } = this.state;
+      const pega = await getMusics(id);
+      this.setState({ infos: [...pega] }, () => {
+        this.setState({ artista: pega[0] });
+        // console.log(artista);
+      });
+    };
+
+    render() {
+      const { infos, artista } = this.state;
+      return (
+        <div data-testid="page-album">
+          <Header />
+          <h1 data-testid="artist-name">{artista.artistName}</h1>
+          <p data-testid="album-name">{artista.collectionName}</p>
+          {
+            infos.map((musica, index) => {
+              if (index > 0) {
+                return (
+                  <MusicCard
+                    trackName={ musica.trackName }
+                    key={ musica.trackName }
+                    previewUrl={ musica.previewUrl }
+                  />
+                );
+              } return null;
+            })
+          }
+        </div>
+      );
+    }
 }
+Album.propTypes = {
+  match: PropTypes.object,
+}.isRequired;
