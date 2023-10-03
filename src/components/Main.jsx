bfe115685f79b685dcbe476c14115979c014e206
Willian Felipe <w.felipebraz@gmail.com>
2023-10-03 17:07:25 -0300

    Routes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Search from '../pages/Search';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import Favorites from '../pages/Favorites';
-import NotFound from '../pages/NotFound';
-import Profile from '../pages/Profile';
-import ProfEdit from '../pages/ProfEdit';
-import Album from '../pages/Album';
 import Login from '../pages/Login';
+import Profile from '../pages/Profile';
+import ProfileEdit from '../pages/ProfileEdit';
+import Search from '../pages/Search';
+import AlbumId from '../pages/AlbumId';
+import NotFound from '../pages/NotFound';
 
-export default class Main extends Component {
+export default class Content extends Component {
   render() {
     return (
-      <Switch>
-        <Route path="/" exact component={ Login } />
-        <Route path="/search" component={ Search } />
-        <Route path="/album/:id" component={ Album } />
-        <Route path="/album" component={ Album } />
-        <Route path="/favorites" component={ Favorites } />
-        <Route path="/profile/edit" component={ ProfEdit } />
-        <Route path="/profile" component={ Profile } />
-        <Route path="*" component={ NotFound } />
-      </Switch>
+      <div>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ AlbumId } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </div>
+
     );
   }
 }
