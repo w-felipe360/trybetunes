837f24b5537c5b4117fbe4b909084ca5eae1af61
Willian Felipe <w.felipebraz@gmail.com>
2023-10-03 17:11:11 -0300

    Página de favoritos

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,54 @@
 import React, { Component } from 'react';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import MusicCard from '../components/MusicCard';
 import Header from '../components/Header';
 
 export default class Favorites extends Component {
-  render() {
-    return (
+  state = {
+    isLoading: false,
+    favoriteSongs: [],
+  };
 
-      <div data-testid="page-favorites">
+  async componentDidMount() {
+    this.getFavorites();
+  }
+
+  getFavorites = async () => {
+    this.setState({ isLoading: true });
+    const musics = await getFavoriteSongs();
+    this.setState({ favoriteSongs: musics, isLoading: false });
+    console.log('musicas', musics);
+  };
+
+  updateFavorites = async () => {
+    await this.getFavorites();
+  };
+
+  render() {
+    const { isLoading } = this.state;
+    const { favoriteSongs } = this.state;
+    console.log('aaaa', favoriteSongs);
+    if (isLoading) {
+      return <p>Carregando...</p>;
+    }
+    if (!favoriteSongs || favoriteSongs.length === 0) {
+      return <p>Não há músicas favoritadas</p>;
+    }
+    return (
+      <div>
         <Header />
-      </div>);
+        {favoriteSongs.map((music) => (
+          <MusicCard
+            key={ music.trackId }
+            trackId={ music.trackId }
+            trackName={ music.trackName }
+            previewUrl={ music.previewUrl }
+            artworkUrl100={ music.artworkUrl100 }
+            updateFavorites={ this.updateFavorites } // Passa a função updateFavorites como propriedade para o MusicCard
+            data={ music } // Adiciona a propriedade "data" ao objeto passado para o componente MusicCard
+          />
+        ))}
+      </div>
+    );
   }
 }
