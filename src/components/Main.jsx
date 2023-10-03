0a4aa9d96b4827b62eac9aa51ffee99f839e7a91
Willian Felipe <w.felipebraz@gmail.com>
2023-10-03 17:09:41 -0300

    Routes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,7 @@ import Login from '../pages/Login';
 import Profile from '../pages/Profile';
 import ProfileEdit from '../pages/ProfileEdit';
 import Search from '../pages/Search';
-import AlbumId from '../pages/AlbumId';
+import Album from '../pages/Album';
 import NotFound from '../pages/NotFound';
 
 export default class Content extends Component {
@@ -15,7 +15,7 @@ export default class Content extends Component {
         <Switch>
           <Route exact path="/" component={ Login } />
           <Route path="/search" component={ Search } />
-          <Route path="/album/:id" component={ AlbumId } />
+          <Route path="/album/:id" component={ Album } />
           <Route path="/favorites" component={ Favorites } />
           <Route exact path="/profile" component={ Profile } />
           <Route path="/profile/edit" component={ ProfileEdit } />
